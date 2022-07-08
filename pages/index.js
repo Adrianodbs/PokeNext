2e8780a69a4471d817ff7a39c5f312cfccc67745
Adriano <adriano_alves_2008@hotmail.com>
2022-07-07 21:11:40 -0300

    componentes de card na home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import Card from './components/card'
 
 export async function getStaticProps() {
   const maxPokemons = 251
@@ -39,7 +40,7 @@ export default function Home({ pokemons }) {
 
       <div className={styles.pokemon_container}>
         {pokemons.map(pokemon => (
-          <p key={pokemon.id}>{pokemon.name}</p>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>
     </>
